556391efac8995fb2f907c64957bc83b2e461d7b
YarAllex <nekromantium@gmail.com>
2020-04-30 13:26:24 +0300

    Fix seting profile

--- src/com/yaralex/bloetooth/headphones/autopair/BluetoothHandler.java
+++ src/com/yaralex/bloetooth/headphones/autopair/BluetoothHandler.java
@@ -12,25 +12,34 @@ public class BluetoothHandler implements Runnable {
     private Process btProcess;
     private BufferedReader bufferedReader;
     private boolean servicesResolved = false;
+    private boolean connectingBt = false;
 
     private Process pacmdProcess;
+    private BufferedReader pacmdBufferedReader;
 
     private String mac = "";
     private boolean starting = true;
 
     public BluetoothHandler(String mac) {
-        this.btProcess = runBluetoothProcess();
+        btProcess = runBluetoothProcess();
+        pacmdProcess = runPacmdProcess();
         this.mac = mac;
     }
 
     @Override
     public void run() {
         try {
-            String line = bufferedReader.readLine();
+            String line = readLine(bufferedReader);
+            System.out.println("TEST2: "+line);
             parseResponse(line);
+            pacmd();
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+    }
+
+    private String readLine(BufferedReader br) throws IOException {
+        return (br != null && br.ready()) ? br.readLine() : null;
     }
 
     private void parseResponse(String response) {
@@ -44,11 +53,13 @@ public class BluetoothHandler implements Runnable {
             servicesResolved = false;
         } else if (response.contains("Connected: no") && !servicesResolved) {
             connectBt();
+        } else if (response.contains("No card found")) {
+
         }
     }
 
     private void connectBt() {
-        System.out.println("Connecting to headphones...");
+        //System.out.println("Connecting to headphones...");
         PrintStream printStream = new PrintStream(btProcess.getOutputStream(), true);
         printStream.println("connect " + mac);
     }
@@ -60,12 +71,33 @@ public class BluetoothHandler implements Runnable {
         Process process = null;
         try {
             process = processBuilder.start();
+            bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
         return process;
+    }
+
+    private Process runPacmdProcess() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("pacmd");
+        processBuilder.redirectErrorStream(true);
+        Process process = null;
+        try {
+            process = processBuilder.start();
+            pacmdBufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return process;
+    }
+
+    private void pacmd() {
+        //System.out.println("Set card profile...");
+        PrintStream printStream = new PrintStream(pacmdProcess.getOutputStream(), true);
+        printStream.println("set-card-profile bluez_card.1C_52_16_5E_04_46 a2dp_sink");
     }
 
     public void start() {
